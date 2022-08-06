5472f55affe87a31bfc330361922c6834dfafb6b
Saket Kumar <saket.kumar.null@gmail.com>
2022-08-06 22:37:45 +0530

    Added Vieo card

--- src/components/content/grid.jsx
+++ src/components/content/grid.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { CgProfile } from "react-icons/cg";
-import { CgSoftwareDownload } from "react-icons/cg";
-
+import ImageCard from "../gridCard/imageCard";
+import VideoCard from "../gridCard/videoCard";
 
 const Grid = () => {
 
     const navigation = useLocation();
-
-    const [gridData, setGridData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch("/getImages",
-            {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                }
-            })
-            .then(response => {
-                if (response.ok) {
-                    response.json().then(json => {
-                        setGridData(json.aImageResponse);
-                    });
-                }
-            });
-    }, []);
+    var Gridcard;
+    if (navigation.pathname === "/videos") {
+        Gridcard = <VideoCard />
+    } else {
+        Gridcard = <ImageCard />
+    }
 
     const getTitle = () => {
         var title = "";
@@ -57,29 +43,7 @@ const Grid = () => {
             <div className=" bg-gradient-to-r from-[#ebebfc] pl-4 ">
                 <h2 className="py-4 items-center font-bold">{getTitle()}</h2>
             </div>
-
-            {/* card */}
-            <div className="md:masonry-3-col lg:masonry-4-col box-border mx-auto before:box-inherit after:box-inherit items-center pt-4 space-y-5">
-
-                {!gridData ?
-                    "" :
-                    gridData.map((tileData, index, obj) => (
-                        <div key={index} className="break-inside rounded-lg shadow-lg rounded-t-lg hover:scale-105 duration-300">
-                            <img src={tileData.src} alt="/" />
-                            <div className="flex justify-between px-4 py-2 items-center object-cover">
-                                <p className="items-center py-2 flex">
-                                    {tileData.alt}
-                                </p>
-                                <span>
-                                    <button className="flex items-center py-1 px-4">
-                                        <CgSoftwareDownload size={22} />
-                                    </button>
-                                </span>
-                            </div>
-                        </div>
-                    ))}
-
-            </div>
+            {Gridcard}
         </div>
     );
 };
---
+++ src/components/gridCard/imageCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { CgSoftwareDownload } from "react-icons/cg";
+
+const ImageCard = () => {
+
+    const [gridData, setGridData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("/getImages",
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    response.json().then(json => {
+                        setGridData(json.aImageResponse);
+                    });
+                }
+            });
+    }, []);
+
+
+
+    return (
+        <div className="md:masonry-3-col lg:masonry-4-col box-border mx-auto before:box-inherit after:box-inherit items-center pt-4 space-y-5">
+
+            {!gridData ?
+                "" :
+                gridData.map((tileData, index, obj) => (
+                    <div key={index} className="break-inside rounded-lg shadow-lg rounded-t-lg hover:scale-105 duration-300">
+                        <img src={tileData.src} alt="/" />
+                        <div className="flex justify-between px-4 py-2 items-center object-cover">
+                            <p className="items-center py-2 flex">
+                                {tileData.alt}
+                            </p>
+                            <span>
+                                <button className="flex items-center py-1 px-4">
+                                    <CgSoftwareDownload size={22} />
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                ))}
+
+        </div>
+    );
+
+};
+
+export default ImageCard;
