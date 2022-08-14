e3796196144352d1c302576882c7cfd9700db9eb
Saket Kumar <saket.kumar.null@gmail.com>
2022-08-14 13:07:14 +0530

    Scroll to top

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,6 +8,11 @@ import {
 } from "react-icons/fa";
 
 const Footer = () => {
+
+  const ScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
   return (
     <div className="w-full py-16 shadow-2xl bg-gradient-to-l from-[#0f2527] to-[#2C5364] text-white ">
       <div className="max-w-none mx-auto flex flex-col px-4">
@@ -25,7 +30,7 @@ const Footer = () => {
             <FaPinterest size={28} className="icon hover:text-red-700" />
           </div>
         </div>
-        
+
         {/* Footer Menu  */}
         <div className="flex justify-between ">
           <ul className="lg:flex">
@@ -53,9 +58,9 @@ const Footer = () => {
       </div>
       {/* back to top */}
       <div className="fixed z-30 bottom-0 right-0 mr-6 mb-6 ">
-        <a href="/" className="text-gray-300 items-center block shadow-md bg-gray-600/80 py-2 px-2 rounded-md hover:text-white ">
+        <a onClick={() => ScrollToTop()} className="text-gray-300 items-center block shadow-md bg-gray-600/80 py-2 px-2 rounded-md hover:text-white ">
           <CgArrowLongUpL size={22} className='hover:scale-105 ' />
-          </a>
+        </a>
       </div>
     </div>
   );
