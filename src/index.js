8206923d6a99c1cd9cba5799896d69875326da8f
Saket Kumar <saket.kumar.null@gmail.com>
2022-09-04 02:18:20 +0530

    regression fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter
+} from 'react-router-dom';
 import './index.css';
 import App from './pages/App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ReactDOM.render( <
+  BrowserRouter >
+  <
+  App / >
+  <
+  /BrowserRouter>,
   document.getElementById('root')
 );
-
-
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,16 @@
 import Content from "../components/content";
 import Footer from "../components/footer";
 import Header from "../components/header";
-
+import React from "react";
 
 function App() {
   return (
     <>
-    
+
       <Header />
-      <Content/>
+      {/* <Content /> */}
       <Footer />
-      
+
     </>
   );
 }
